cc145884f75bed720875561b52608bb980bafd9c
Santiago Montoya <santiagomontoya317@gmail.com>
2023-04-30 15:17:11 -0500

    Revert "Merge branch 'felipe_sepulveda'"
    This reverts commit a5de1207d5ccbac258ed780eb194cecb95052a4f, reversing
    changes made to 0bc0ccdd015dbd8f2ff762726253292b4ed77a7a.

--- src/main.jsx
+++ src/main.jsx
@@ -2,41 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Navbar from './components/navbar/Navbar'
-import RestaurantTables from './components/restaurant-tables/RestaurantTables'
-import ReservationForm from './components/reservation-form/reservationForm'
+import RestaurantTargets from './components/restaurant-targets/RestaurantTargets'
+import SponsorBrands from './components/sponsor-brands/SponsorBrands'
 import Footer from './components/footer/Footer'
-import BannerCard from './assets/banner.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <Navbar />
     <main>
-      <section className='container-banner'>
-      </section>
-
-      <section className='container-card'>
-      <div className="container-tables">
-      <RestaurantTables />
+      <h2 className='subtitle'>¡Los mas elegidos!</h2>
+      <div className='targets'>
+        <RestaurantTargets />
+        <RestaurantTargets />
+        <RestaurantTargets />
+        <RestaurantTargets />
+        <RestaurantTargets />
       </div>
-
-      <div className="container-form">
-      <ReservationForm />
+      <h2 className='subtitle subtitle-2'>Nuestras marcas aliadas</h2>
+      <div className='sponsors targets'>
+        <SponsorBrands />
+        <SponsorBrands />
+        <SponsorBrands />
+        <SponsorBrands />
+        <SponsorBrands />
       </div>
-      </section>
-      
-      <section className='information'>
-      <h2 className='subtitle-2'>¡IMPORTANTE!</h2>
-
-      <div className="circles">
-        <div className="green-circle">
-        </div>
-          <p className='text'>Sí se encuentra este color significa que esta <strong> LIBRE.</strong></p>
-      
-        <div className="red-circle">
-        </div>
-        <p className='text'>Sí se encuentra este color significa que esta <strong>OCUPADO.</strong></p>
-      </div>
-      </section>
     </main>
     <Footer />
   </>
