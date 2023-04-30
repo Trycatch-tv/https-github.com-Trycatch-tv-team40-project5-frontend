20499984161acbdc225e1b3cac861890b647ce40
Santiago Montoya <santiagomontoya317@gmail.com>
2023-04-30 15:35:25 -0500

    React router implementado

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Logo from "../../assets/Logo.svg";
 import "./navbar.css";
 import Modal from "../Modals/Modal";
 import ListTable from "../list-table/ListTable";
+import { Link } from "react-router-dom";
 
 const reservations = [
   {
@@ -212,7 +213,11 @@ function Navbar() {
         </div>
       </Modal>
       <div className="nav-btns">
-        <button className="nav-btn--reserve">Reserva Ya!</button>
+        <button className="nav-btn--reserve">
+          <Link className="nav-link" to="/reservation">
+            Reserva Ya!
+          </Link>
+        </button>
         <button className="nav-btn--session">Iniciar Sesion</button>
       </div>
     </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
 import Reservation from "./components/reservation/Reservation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <BrowserRouter>
     <Navbar />
     <main>
-      <Home />
-      <Reservation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservation" element={<Reservation />} />
+      </Routes>
     </main>
     <Footer />
-  </>
+  </BrowserRouter>
 );
