06effcf66364fea643c22710af5293a70bafe765
Santiago Montoya <santiagomontoya317@gmail.com>
2023-04-30 15:25:25 -0500

    Creacion componente reservation

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Navbar from './components/navbar/Navbar'
-import RestaurantTables from './components/restaurant-tables/RestaurantTables'
-import ReservationForm from './components/reservation-form/reservationForm'
-import Footer from './components/footer/Footer'
-import BannerCard from './assets/banner.png'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Reservation from "./components/reservation/Reservation";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Navbar />
     <main>
-      <section className='container-banner'>
-      </section>
-
-      <section className='container-card'>
-      <div className="container-tables">
-      <RestaurantTables />
-      </div>
-
-      <div className="container-form">
-      <ReservationForm />
-      </div>
-      </section>
-      
-      <section className='information'>
-      <h2 className='subtitle-2'>¡IMPORTANTE!</h2>
-
-      <div className="circles">
-        <div className="green-circle">
-        </div>
-          <p className='text'>Sí se encuentra este color significa que esta <strong> LIBRE.</strong></p>
-      
-        <div className="red-circle">
-        </div>
-        <p className='text'>Sí se encuentra este color significa que esta <strong>OCUPADO.</strong></p>
-      </div>
-      </section>
+      <Reservation />
     </main>
     <Footer />
   </>
-)
+);
